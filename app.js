f634a58ee424e051765f9ec10811ff0b2523e258
sonalb22 <bhartisonal522@gmail.com>
2021-06-22 14:40:10 +0530

    final

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ passport.serializeUser(function(user, done) {
  passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "https://peaceful-mesa-95557.herokuapp.com/auth/google/blogs",
+  callbackURL: "https://desolate-tundra-52340.herokuapp.com/auth/google/blogs",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 //google providing acessToken to acess info of user
